31408d827e91e2b53451db3e7b7724cb45b36d83
sreenaths <somarajapuram@gmail.com>
2014-10-06 14:30:00 +0530

    Error handling & messaging

--- server.js
+++ server.js
@@ -13,30 +13,41 @@ var http = require('http'),
 
 http.createServer(function(request, response) {
 	var filename = path.join(process.cwd(), 'data/' + url.parse(request.url).pathname);
+	
+	process.stdout.write("Resuested for " + filename + " :");
 
 	fs.exists(filename, function(exists) {
 		if(!exists) {
 			response.writeHead(404, {'Content-Type': 'text/plain'});
 			response.write('404 Not Found\n');
+			console.log(" Err:Not Found");
 			response.end();
-			return;
 		}
+		else {
+			if (fs.statSync(filename).isDirectory()) filename = path.join(filename, '/index.json');
 
-		if (fs.statSync(filename).isDirectory()) filename += '/index.json';
-
-		fs.readFile(filename, "binary", function(err, file) {
-			if(err) {				
-				response.writeHead(500, {"Content-Type": "application/json"});
-				response.write(err + "\n");
-				response.end();
-				return;
-			}
-
-			response.writeHead(200);
-			response.write(file, "binary");
-			response.end();
-		});
+			fs.readFile(filename, "binary", function(err, file) {
+				if(err) {				
+					response.writeHead(500, {"Content-Type": "application/json"});
+					response.write(err + "\n");
+					console.log(" Err:" + err);
+					response.end();
+				}
+				else {
+					response.writeHead(200);
+					response.write(file, "binary");
+					console.log(" Success!");
+					response.end();
+				}
+			});
+		}
 	});
-}).listen(port);
+}).listen(port, function(err){
+	if(err) {
+		console.log("Unable to listen : ", err);
+	}
+	else {
+		console.log("Timeline Server running at http://localhost:" + port + "/\nUse CTRL+C to shutdown");
+	}
+});
 
-console.log("Timeline Server running at http://localhost:" + port + "/\nUse CTRL+C to shutdown");
