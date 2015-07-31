7fab5173f14b27616b3b5509fc44c99309a25916
Sreenath Somarajapuram <sree@apache.org>
2015-08-01 04:51:26 +0530

    Added DAG zip uploader

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var http = require('http'),
   path = require('path'),
   fs = require('fs'),
   qs = require('querystring'),
+  formidable = require('formidable'),
+  unzip = require('unzip2'),
 
   cache = {},
   uiDomain = process.argv[2] || 'http://localhost:9001',
@@ -128,7 +130,121 @@ function readData(requestPath, query, callback) {
   });
 }
 
-http.createServer(function(request, response) {
+function dagUpload(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end("<style>input{font-size:1.5em; padding: 1em;}</style>\
+      <form action='/uploader' method='post' enctype='multipart/form-data' style='text-align:center;'>\
+        </br><h1>Choose DAG zip file(s) to upload</h1>\
+        <input type='file' name='upload' multiple='multiple' accept='.zip'>\
+        <input type='submit' value='Upload'>\
+      </form>");
+}
+
+function extractData(dest, src) {
+  if(src.application) dest.applications.push(src.application);
+  if(src.dag) dest.dags.push(src.dag);
+
+  if(src.vertices) dest.vertices = dest.vertices.concat(src.vertices);
+  if(src.tasks) dest.tasks = dest.tasks.concat(src.tasks);
+  if(src.task_attempts) dest.task_attempts = dest.task_attempts.concat(src.task_attempts);
+}
+
+function extractFiles(zipFiles, callback) {
+  var data = {
+    applications: [],
+    dags: [],
+    vertices: [],
+    tasks: [],
+    task_attempts: []
+  },
+  completeCount = 0;
+
+  if(!Array.isArray()) zipFiles.upload = [zipFiles.upload];
+
+  zipFiles.upload.forEach(function (file){
+    var unzipDir = file.path + "_dir/";
+
+    fs.createReadStream(file.path).
+    pipe(unzip.Extract({ path: unzipDir }).
+      on('close', function() {
+        fs.readdir(unzipDir, function(err, files){
+          if (err) throw err;
+          files.forEach(function(file){
+              var json = JSON.parse(fs.readFileSync(unzipDir + file, 'utf8'));
+              extractData(data, json);
+          });
+
+          completeCount++;
+
+          if(completeCount == zipFiles.upload.length) {
+            callback(data);
+          }
+        });
+      })
+    );
+  });
+}
+
+function appendJSONEntities(file, data) {
+  var json = {entities: []};
+  try {
+    json = JSON.parse(fs.readFileSync(file, 'utf8'));
+  }
+  catch(e){}
+
+  json.entities = json.entities.concat(data);
+
+  fs.writeFile(file, JSON.stringify(json));
+}
+
+function saveData(data) {
+  console.log(data);
+  appendJSONEntities('data/ws/v1/timeline/TEZ_DAG_ID/index.json', data.dags);
+  appendJSONEntities('data/ws/v1/timeline/TEZ_APPLICATION/index.json', data.applications);
+
+  appendJSONEntities('data/ws/v1/timeline/TEZ_VERTEX_ID/index.json', data.vertices);
+  appendJSONEntities('data/ws/v1/timeline/TEZ_TASK_ID/index.json', data.tasks);
+  appendJSONEntities('data/ws/v1/timeline/TEZ_TASK_ATTEMPT_ID/index.json', data.task_attempts);
+}
+
+function sendResponse(response, statusMsg) {
+  response.writeHead(200, {'content-type': 'text/html'});
+  response.write("<body style='text-align:center'></br>\
+      <h1>Dag(s) " + statusMsg + "</h1>\
+      <a href='dagupload'><h4>Back to DAG upload page</h4></a>\
+    </body>");
+  response.end();
+}
+
+function uploader(request, response) {
+  var form = new formidable.IncomingForm();
+
+  form.multiples = true;
+
+  form.parse(request, function(err, fields, files) {
+    console.log(files);
+
+    if(err) sendResponse(response, "upload failed!");
+
+//    try {
+      extractFiles(files, function (data) {
+        try {
+          saveData(data);
+          cache = {};
+          sendResponse(response, "uploaded successfully.");
+        }
+        catch(e) {
+          sendResponse(response, "save failed!");
+        }
+      });
+    // }
+    // catch(e) {
+    //   sendResponse(response, "extraction failed!");
+    // }
+  });
+}
+
+function webService(request, response) {
   var parsedUrl = url.parse(request.url),
       requestPath = path.join(process.cwd(), 'data/', parsedUrl.pathname),
       query = qs.parse(parsedUrl.query);
@@ -157,7 +273,23 @@ http.createServer(function(request, response) {
     console.log("Rquested for " + requestPath + " : " + log);
     response.end();
   });
+}
 
+http.createServer(function(request, response) {
+  var pathname = url.parse(request.url, true).pathname;
+
+  switch (true) {
+    case /\/dagupload/.test(pathname):
+      dagUpload(request, response);
+      break;
+
+    case /\/uploader/.test(pathname):
+      uploader(request, response);
+      break;
+
+    default:
+      webService(request, response);
+  }
 }).listen(port, function(err){
   if(err) {
     console.log("Unable to listen : ", err);
